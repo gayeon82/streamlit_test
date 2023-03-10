a1e99e1d3705264e23ce71549a711cd64e07bab1
emily-LSA <54883029+emily-LSA@users.noreply.github.com>
2023-03-10 09:56:07 +0900

    test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 # app.py
 import streamlit as st
 
-st.title('streamlit test')
+st.title('streamlit test!')
 
-st.write('안녕하세요 수현님? 화면이 잘 보이면 팀즈를 날려주세여!')
+
+input_user_name = st.text_input(label="User Name", value="default value")
+input_password = st.text_input(label="Password", value="default value")
+
+if (st.button("Confirm")) & (input_password == "mezzo"):
+    con = st.container()
+    con.caption("Result")
+    con.write(f"Hello~ {str(input_user_name)}")
+else:
+    con = st.container()
+    con.write("비밀번호가 틀렸습니다. 확인 후 다시 입력해주세요.")  
+
+
+# tab1, tab2= st.tabs(['Tab A' , 'Tab B'])
+    
+# with tab1:
+#   #tab A 를 누르면 표시될 내용
+#   st.write('hello')
+    
+# with tab2:
+#   #tab B를 누르면 표시될 내용 
+#   st.write('hi')
